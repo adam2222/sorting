e2e94de4adf65bd0f23b001b7b38a71a7c3ba90b
Adam M. Intrator <adam.intra2r@gmail.com>
2016-11-03 13:03:17 -0400

    second commit

---
+++ bubblesort.js
@@ -0,0 +1,20 @@
+function bubbleSort(array){
+
+  for (var lastUnsorted = array.length - 1; lastUnsorted > 0; lastUnsorted--){
+
+    for (var i = 0; i < lastUnsorted; i++) {
+      array [i]
+    }
+
+  }
+
+}
+
+function isLessThan(a, b){
+  return a < b;
+}
+
+function swap(firstIndex, secondIndex, array){
+  var temp = array[firstIndex];
+  
+}
---
+++ bubblesort.spec.js
@@ -0,0 +1,22 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+  });
+
+  it('handles an array of one element', function(){
+    expect( bubbleSort([1])).toEqual([1]);
+  });
+
+  it('handles an array of two elements', function(){
+    expect( bubbleSort([2, 1])).toEqual([1, 2]);
+  });
+
+  it('handles an array of multiple elements with one unsorted elements', function(){
+    expect( bubbleSort([1, 2, 4, 3, 5, 6, 7])).toEqual([1, 2, 3, 4, 5, 6, 7]);
+  });
+
+  it('handles an array of multiple elements with multiple unsorted elements', function(){
+    expect( bubbleSort([2, 1, 5, 4, 7, 6])).toEqual([1, 2, 4, 5, 6, 7]);
+  });
+
+});
